bcfbf4f40902e5905548af17658686191650215b
JumbooWong <493182574@qq.com>
2018-12-23 22:54:48 +0800

    更新

--- vietRegn.py
+++ vietRegn.py
@@ -7,7 +7,7 @@ import re
 def getUnicode(source):
     # charList  = []
     source_2 = source.encode('unicode_escape').decode()
-    print(source,source_2)
+    # print(source,source_2)
     charList = re.findall('\\\\u\w{4}|\\\\x\w{2}',source_2)
     return charList
 
@@ -19,31 +19,45 @@ def getVietnamese(source):
         print('正常1：', source.decode('gb2312'))
     except:
         try:
-            # 排除画像中有繁体字
+            # 繁体字
             source = source.encode('big5')
             print('正常2：', source.decode('big5'))
         except:
             try:
-                source = source.encode('unicode')
-                print('异常：', source)
+                res = langid.classify(source)
+                if res[0] == 'vi':
+                    print('越南语：', source)
             except:
                 #     source = source.encode('unicode_escape').decode().encode('windows-1258')
-                #     # source.encode('unicode_escape').encode('windows-1258')
-                print(123, source)
-                print()
+                pass
                 #     print('异常：', source.decode('windows-1258'))
 import langid
+import time
 if __name__ == '__main__':
     # yn = 'Thời tiết hâôm nay thế nào'
     # res = yn.encode('unicode_escape').decode('utf-16').encode('windows-1258')
     # print(res)
     f = open('data/vietnamese',encoding='utf-8')
-    for source in f.readlines():
-        source = source.strip()
+    start = time.time()
+    for i in range(1000):
         source = 'Thời tiết hâôm nay thế nào'
+        res = getUnicode(source)
+        for each in res:
+            for j in range(50):
+                if each == 1:break
+
+
+        # print(res[0],source)
+    # for source in f.readlines()[:3]:
+    #     source = source.strip()
+    #     getVietnamese(source)
+    end = time.time()
+    print('1000条',end-start)
+
+
         # res = getUnicode(source)
-        # res = langid.classify(source)
-    print(res)
+        #
+
         #'gb2312'
 
 
